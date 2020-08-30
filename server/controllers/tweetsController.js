92960b7b3f27dbcc2363cf7409fce0965fc2d85e
bartek9105 <bartek9105@gmail.com>
2020-08-30 12:22:54 +0200

    use sequelize define for user model, sync user model, delete knex package

--- server/controllers/tweetsController.js
+++ server/controllers/tweetsController.js
@@ -1,14 +1,6 @@
-const knex = require('../knex')
-
 exports.getTweets = async (req, res, next) => {
     try {
-        const timeline = await knex
-        .select('users.email', 'tweets.id', 'tweets.body', 'tweets.type', 'tweets.original_tweet_id')
-        .from('users')
-        .join('followers', 'users.id', '=', 'followers.user_id')
-        .join('tweets', 'followers.following_id', '=', 'tweets.user_id')
-        .where('users.id', 1)
-        res.send(timeline)
+        res.send('Get route')
     } catch (error) {
         console.log(error)
     }
@@ -16,13 +8,7 @@ exports.getTweets = async (req, res, next) => {
 
 exports.addTweet = async (req, res, next) => {
     try {
-        const { tweetBody } = req.body
-        await knex('tweets').insert({
-            user_id: 2,
-            body: tweetBody,
-            type: 'reply'
-        })
-        res.send({ body: tweetBody })
+        res.send('Add route')    
     } catch (error) {
         console.log(error)
     }
--- server/models/User.js
+++ server/models/User.js
@@ -1,17 +1,19 @@
-class User extends Model {}
+const { DataTypes } = require('sequelize');
+const db = require('../utils/dbConnection')
 
-User.init({
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    timestamps: true,
-    createdAt: true
-}, {
-    sequelize,
-    modelName: 'User'
+const User = db.define('User', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
 })
+
+User.sync().then(() => {
+    console.log('User model synced')
+})
+
+module.exports = User
